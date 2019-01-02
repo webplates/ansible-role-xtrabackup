74cdf888717b96e6e3f82132ae0e01dc6d29d9e0
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-01-02 22:07:40 +0100

    Add system user and rename test scenario

---
+++ molecule/all/tests/test_default.py
@@ -0,0 +1,37 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_xtrabackup_is_installed(host):
+    xtrabackup = host.package("percona-xtrabackup-80")
+
+    assert xtrabackup.is_installed
+    assert xtrabackup.version.startswith("8")
+
+
+def test_xtrabackup_system_user_is_created(host):
+    user = host.user("xtrabackup")
+
+    assert user.shell == "/usr/sbin/nologin"
+
+
+def test_xtrabackup_my_cnf_is_created(host):
+    user = host.user("xtrabackup")
+    f = host.file(user.home + "/.my.cnf")
+
+    assert f.exists
+    assert f.user == user.name
+    assert f.group == user.group
+    assert f.mode == 0o600
+    assert f.contains('user="bkpuser"')
+    assert f.contains('password="bkpuser"')
+
+
+def test_xtrabackup_mysql_user_is_created(host):
+    cmd = host.run("mysql -u bkpuser -pbkpuser -e 'show databases;'")
+
+    assert cmd.rc == 0
